44a705f95a0dd3da09c6e702f3fc9d1b018f25bd
Felipe Genuino <design@felipegenuino.com>
2020-04-12 13:06:01 -0300

    Implamenta validaç;ao de QUERY na rota incidents

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -26,7 +26,11 @@ routes.get('/profile', celebrate({
     }).unknown(),
 }), ProfileController.index);
 
-routes.get('/incidents', IncidentsController.index);
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentsController.index);
 routes.post('/incidents', IncidentsController.create);
 routes.delete('/incidents/:id', celebrate({
     [Segments.PARAMS]: Joi.object().keys({
