86723b0f727b8d271e5664f4e6dd3f876e787db1
Felipe Genuino <design@felipegenuino.com>
2020-04-12 21:07:34 -0300

    Create integration test

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,16 +1,30 @@
-const request = require('supertest');
 const app = require('../../src/app');
+const request = require('supertest');
+const connection = require('../../src/database/connection')
 
-describe('ONG', ()=>{
-    it('should be able to create a new ONG', async ()=>{
+describe('ONG', () => {
+    beforeEach( async ()=>{
+        await connection.migrate.latest()
+    })
+
+    afterAll(async ()=>{
+        await connection.destroy()
+    })
+
+    it('should be able to create a new ONG', async () => {
         const response = await request(app)
         .post('/ongs')
         .send({
-            name: "papai do cr",
-            email: "email@lardezulma.com.br",
+            name: "apad",
+            email: "email@test.com",
             whatsapp: "48999113048",
             city: "São José",
             uf: "sc"
         });
+
+        console.log(response.body);
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8)
     });
+
 });
